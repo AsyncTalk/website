99d189d255d77c255032a7b18650723a9914fe7e
AnnatarHe <annatar.he+github@gmail.com>
2022-06-29 16:53:36 +0800

    feat(init): init project

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+
+// markup
+const IndexPage = () => {
+  return (
+  <main className='w-screen h-screen flex items-center justify-center'>
+    <div>
+    <span className='text-8xl text-center w-full block'>
+    👨🏻‍💻
+    </span>
+    <h1 className='text-7xl mt-3'>Async Talk</h1>
+    </div>
+  </main>
+  )
+}
+
+export default IndexPage
